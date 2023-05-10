7bf5ea0ba8c6691eb5de0a01f30135ffb59309d8
Aslan Inalov <aslaninalov@yandex.ru>
2023-05-10 22:36:20 +0300

    pr13

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const usersRouter = require('express').Router();
+const {
+  createUser,
+  getUsers,
+  getUser,
+  updateProfile,
+  updateAvatar,
+} = require('../controllers/users');
+
+usersRouter.post('/', createUser);
+usersRouter.get('/', getUsers);
+usersRouter.get('/:id', getUser);
+usersRouter.patch('/me', updateProfile);
+usersRouter.patch('/me/avatar', updateAvatar);
+
+module.exports = usersRouter;
